92c3a9e0a2859e6f23649bde82ea3dad00d60b93
Jyotirmoy Maschatak <jmaschatak@deloitte.com>
2024-05-01 15:12:58 +0530

    full diamond pattern

---
+++ DSA Problems/GFG-full-diamond-pattern/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int n = 5;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - i - 1; j++)
+        Console.Write(" ");
+
+    for (int j = 0; j < (2 * i + 1); j++)
+        Console.Write("*");
+
+    for (int j = 0; j < n - i - 1; j++)
+        Console.Write(" ");
+
+    Console.WriteLine();
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j > 0; j--)
+        Console.Write(" ");
+
+    for (int j = 2*(n-i) -1; j > 0; j--)
+        Console.Write("*");
+
+    for (int j = i; j > 0; j--)
+        Console.Write(" ");
+
+    Console.WriteLine();
+}
